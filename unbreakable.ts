97003899a278daa550fe7023fdca75429fb80a90
Leo Gaskin <leo.gaskin@le0.gs>
2022-11-21 14:46:19 +0100

    Increase timeout

--- unbreakable.ts
+++ unbreakable.ts
@@ -90,6 +90,6 @@ Promise.all(references).then((unsorted) =>
       return stack
         .then(() => archive(elem.href))
         .then(() => console.log("Archived:", elem.href))
-        .then(() => new Promise((resolve) => setTimeout(resolve, 11000)));
+        .then(() => new Promise((resolve) => setTimeout(resolve, 15000)));
     }, Promise.resolve())
 );
